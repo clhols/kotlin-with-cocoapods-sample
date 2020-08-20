937529ca1bcc1d67bf81f3dbb54c7c94002496e0
Claus Holst <clhol@danskebank.dk>
2020-08-20 17:44:04 +0200

    Produce name clash

--- afnetworking/build.gradle.kts
+++ afnetworking/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.4.255-SNAPSHOT"
-    kotlin("native.cocoapods") version "1.4.255-SNAPSHOT"
+    kotlin("multiplatform") version "1.4.0"
+    kotlin("native.cocoapods") version "1.4.0"
 }
 
 group = "org.jetbrains.kotlin.sample"
@@ -23,15 +23,6 @@ kotlin {
 
         //Example of usage remote Cocoapods dependency from Cocoapods repository
         pod("AFNetworking", "~> 4.0.0")
-
-        //Example of usage remote Pod declared as Subspec
-        pod("SDWebImage/MapKit")
-
-        //Example of usage local Cocoapods dependency
-        pod("pod_dependency", "1.0", project.file("../pod_dependency/pod_dependency.podspec"))
-
-        //Example of usage local Pod declared as Subspec
-        pod("subspec_dependency/Core", "1.0", project.file("../subspec_dependency/subspec_dependency.podspec"))
 
     }
 }
